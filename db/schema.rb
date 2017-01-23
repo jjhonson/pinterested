ebbfa34985c55604efdf11d4b0ccbe138d5e90d1
Jason Jhonson <jasonsjhonson@gmail.com>
2017-01-23 03:05:30 +0000

    Adjustments to AWS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107232613) do
+ActiveRecord::Schema.define(version: 20170123030333) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
